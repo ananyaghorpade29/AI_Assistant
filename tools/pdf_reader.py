80808591e3b1fca00205c2eb05a35caef524723b
ananyaghorpade29 <ananyaghorpade29@gmail.com>
2026-07-28 03:59:24 +0000

    exceptional handling

--- tools/pdf_reader.py
+++ tools/pdf_reader.py
@@ -18,12 +18,15 @@ def read_pdf(pdf_path:Path) -> str:
         A single string containing all extracted text.
     """
 
-	reader = PdfReader(pdf_path)
-	text =""
- 	for page in reader.pages:
-		page_text = page.extract_text()
+	try:
+		reader = PdfReader(pdf_path)
+		text =""
+ 		for page in reader.pages:
+			page_text = page.extract_text()
 
-		if page_text:
-			text += page_text +"\n"
-	return text
+			if page_text:
+				text += page_text +"\n"
+		return text
 
+	except Exception as error:
+		return f"Failed to read PDF: {error}"
